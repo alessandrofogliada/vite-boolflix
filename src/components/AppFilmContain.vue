a7d2f95fbce305f3b019d926c06948fdd3a069f0
alessandrofogliada <fogliada.a@gmail.com>
2024-07-11 12:53:13 +0200

    1.0.0 Base layout

---
+++ src/components/AppFilmContain.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name:'AppFilmContain'
+}
+
+</script>
+
+
+<template>
+    <!-- container principale  -->
+    <div class="container">
+        <!-- container per accentrare i contenuti  -->
+        <div class="containeraccentratore">
+
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.container{
+    width: 1000px;
+    min-height: 400px;
+    background-color: lightslategrey;
+}
+
+.containeraccentratore{
+    width: 800px;
+    margin: 0 auto;
+    min-height: 750px;
+    background-color: brown;
+    padding-top: 20px;
+}
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name:'AppHeader'
+}
+
+</script>
+
+
+<template>
+
+    <div class="intestazione">
+
+        <!-- Titolo  -->
+        <h1>Boolflix</h1>
+
+        <!-- Barra e tasto di ricerca  -->
+        <div class="barradiricerca">
+            <input type="text" placeholder="Search Film">
+            <button>Search</button>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.intestazione{
+    width: 1000px;
+    height: 120px;
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+}
+h1{
+    color: red;
+}
+
+.barradiricerca{
+    display: flex;
+    align-items: center;
+}
+
+</style>
